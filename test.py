6bf99e8987dd9b79df8ce15737b07dd6e2c767af
Bejayyy <joro.igot.swu@phinmaed.com>
2024-08-02 17:38:18 +0800

    First Commit

---
+++ test.py
@@ -0,0 +1,17 @@
+print ('Hello world')
+
+number_one = 10
+number_two = 13
+
+first_name = 'Kurt'
+
+char_sample = 'c'
+
+people_age = {
+    'kurt' : 20,
+    'mel' : 20,
+    'Nat' : 21
+}
+
+total = number_one + number_two
+print('Result is ' , total)
